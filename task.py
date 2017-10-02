6f085478c606297b77bdcfc30ed75b8ba1d89b3e
saipavans <sp403250@dal.ca>
2017-10-02 12:52:11 -0300

    opend file using 'with'

--- task.py
+++ task.py
@@ -6,20 +6,22 @@ class CrimeLister():
     crime_counter = dict()
 
     def __init__(self, filename):
-        self.fin = open(filename)
+        self.filename = filename
 
     def populate_record(self):
-        for line in self.fin:
-            if line.find("DATE") == -1: ## EXCLUDE HEADER
-                line_content = line.strip()
-                line_content = line_content.split(",")
-                crime_type = line_content[len(line_content)-1]
-                crime_id = line_content[len(line_content)-2]
-                crime_key = crime_type + "," + crime_id
-                self.crime_counter[crime_key] = self.crime_counter.get(crime_key + "", 0) + 1
-        for crime in self.crime_counter.keys():
-            crime_type_id = crime.split(",")
-            self.crime_records.append([crime_type_id[0],crime_type_id[1],self.crime_counter[crime]])
+        with open(self.filename) as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.find("DATE") == -1: ## EXCLUDE HEADER
+                    line_content = line.strip()
+                    line_content = line_content.split(",")
+                    crime_type = line_content[len(line_content)-1]
+                    crime_id = line_content[len(line_content)-2]
+                    crime_key = crime_type + "," + crime_id
+                    self.crime_counter[crime_key] = self.crime_counter.get(crime_key + "", 0) + 1
+            for crime in self.crime_counter.keys():
+                crime_type_id = crime.split(",")
+                self.crime_records.append([crime_type_id[0],crime_type_id[1],self.crime_counter[crime]])
 
 
     def print_crime_records(self):
